91bd9670ee7fdb919090606f68e53dd835f346ab
AsmoMictian <asmodeus.mictian@gmail.com>
2025-06-09 01:10:04 -0400

    Start of CH2L2

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
 
+def main():
+    node = TextNode("This is some anchor text", TextType.LinksType, "https://mywebsitehere.com")
+    print(node)
+
+if __name__ == "__main__":
+    main()
